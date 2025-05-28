d5e35e72b67e1bcb117901e89b8549d124b82d9f
feJustino <fwxjustino@gmail.com>
2025-05-28 07:33:16 -0300

    feat(auth): implement authentication management and integrate with Header component

--- app/components/landing-page/header.tsx
+++ app/components/landing-page/header.tsx
@@ -1,6 +1,9 @@
+import { manageAuth } from '@/app/actions/manage-auth';
 import Button from '@/app/components/ui/button';
+import { auth } from '@/app/lib/auth';
 
-export function Header() {
+export async function Header() {
+  const session = await auth();
   return (
     <div className="absolute top-0 left-0 right-0 max-w-7xl mx-auto flex items-center justify-between py-10 z-10">
       <div className="flex items-center gap-4">
@@ -8,8 +11,10 @@ export function Header() {
         <h3 className="text-white text-2xl font-bold">Projectinbio</h3>
       </div>
       <div className="flex items-center gap-4">
-        <Button>Minha Pagina</Button>
-        <Button>Sair</Button>
+        {session && <Button>Minha Pagina</Button>}
+        <form action={manageAuth}>
+          <Button>{session ? 'Sair' : 'Login'}</Button>
+        </form>
       </div>
     </div>
   );

1f75f2b0125c4adbea009459206a8f5ec6564ac8
feJustino <fwxjustino@gmail.com>
2025-05-28 18:29:13 -0300

    feat(new-project): implement project creation with image upload and compression

--- app/(pages)/[profileId]/new-project.tsx
+++ app/(pages)/[profileId]/new-project.tsx
@@ -1,14 +1,69 @@
 'use client';
 
+import { createProject } from '@/app/actions/create-project';
 import Button from '@/app/components/ui/button';
 import Modal from '@/app/components/ui/modal';
 import TextArea from '@/app/components/ui/text-area';
 import TextInput from '@/app/components/ui/text-input';
+import { compressFiles } from '@/app/lib/utils';
 import { ArrowUpFromLine, Plus } from 'lucide-react';
-import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { startTransition, useState } from 'react';
 
 function NewProject({ profileId }: { profileId: string }) {
+  const router = useRouter();
+
   const [isOpen, setIsOpen] = useState(false);
+  const [isCreatingProject, setIsCreatingProject] = useState(false);
+  const [projectName, setProjectName] = useState<string>('');
+  const [projectUrl, setProjectUrl] = useState<string>('');
+  const [projectDescription, setProjectDescription] = useState<string>('');
+  const [projectImage, setProjectImage] = useState<string | null>('');
+
+  function triggerImageInput(id: string) {
+    document.getElementById(id)?.click();
+  }
+
+  function handleImageChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const file = e.target.files?.[0] ?? null;
+    if (file) {
+      const imageUrl = URL.createObjectURL(file);
+      setProjectImage(imageUrl);
+    }
+  }
+
+  function handleCloseModal() {
+    setIsOpen(false);
+  }
+
+  async function handleSaveProject() {
+    setIsCreatingProject(true);
+    const imageInput = document.getElementById(
+      'imageInput'
+    ) as HTMLInputElement;
+    if (imageInput.files) {
+      const compressedFiles = await compressFiles(Array.from(imageInput.files));
+
+      const formData = new FormData();
+      formData.append('file', compressedFiles[0]);
+      formData.append('profileId', profileId);
+      formData.append('projectName', projectName);
+      formData.append('projectDescription', projectDescription);
+      formData.append('projectUrl', projectUrl);
+
+      await createProject(formData);
+      startTransition(() => {
+        setIsOpen(false);
+        setProjectName('');
+        setProjectDescription('');
+        setProjectUrl('');
+        setProjectImage(null);
+        setIsCreatingProject(false);
+        router.refresh();
+      });
+    }
+  }
+
   return (
     <>
       <button
@@ -19,14 +74,33 @@ function NewProject({ profileId }: { profileId: string }) {
         <span>Novo projeto</span>
       </button>
       <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
-        <div className="bg-background-primary p-8 rounded-[20px] flex flex-col justify-between gap-10">
+        <div
+          onKeyDown={e => e.key === 'Enter' && handleSaveProject()}
+          className="bg-background-primary p-8 rounded-[20px] flex flex-col justify-between gap-10"
+        >
           <p className="text-white font-bold text-xl">Novo Projeto</p>
           <div className="flex gap-10">
             <div className="flex flex-col items-center gap-3 text-xs">
               <div className="w-[100px] h-[100px] rounded-xl bg-background-tertiary overflow-hidden">
-                <button className="w-full h-full">100x100</button>
+                {projectImage ? (
+                  <img
+                    src={projectImage}
+                    alt="Imagem do projeto"
+                    className="object-cover object-center"
+                  />
+                ) : (
+                  <button
+                    onClick={() => triggerImageInput('imageInput')}
+                    className="w-full h-full"
+                  >
+                    100x100
+                  </button>
+                )}
               </div>
-              <button className="flex text-white items-center gap-2">
+              <button
+                className="flex text-white items-center gap-2"
+                onClick={() => triggerImageInput('imageInput')}
+              >
                 <ArrowUpFromLine className="size-4" />
                 <span>Adicionar imagem</span>
               </button>
@@ -35,6 +109,7 @@ function NewProject({ profileId }: { profileId: string }) {
                 id="imageInput"
                 accept="image/*"
                 className="hidden"
+                onChange={handleImageChange}
               />
             </div>
             <div className="flex flex-col gap-4 w-[293px]">
@@ -42,7 +117,10 @@ function NewProject({ profileId }: { profileId: string }) {
                 <label htmlFor="project-name" className="text-white font-bold">
                   Titulo do projeto
                 </label>
-                <TextInput id="project-name" />
+                <TextInput
+                  onChange={e => setProjectName(e.target.value)}
+                  id="project-name"
+                />
               </div>
               <div className="flex flex-col gap-1">
                 <label
@@ -55,19 +133,28 @@ function NewProject({ profileId }: { profileId: string }) {
                   id="project-description"
                   placeholder="De uma breve descrição do seu projeto"
                   className="h-36"
+                  onChange={e => setProjectDescription(e.target.value)}
                 />
               </div>
               <div className="flex flex-col gap-1">
                 <label htmlFor="project-url" className="text-white font-bold">
                   URL do projeto
                 </label>
-                <TextInput id="project-url" type="url" />
+                <TextInput
+                  onChange={e => setProjectUrl(e.target.value)}
+                  id="project-url"
+                  type="url"
+                />
               </div>
             </div>
           </div>
           <div className="flex gap-4 justify-end">
-            <button className="font-bold text-white">Voltar</button>
-            <Button>Salvar</Button>
+            <button className="font-bold text-white" onClick={handleCloseModal}>
+              Voltar
+            </button>
+            <Button onClick={handleSaveProject} disabled={isCreatingProject}>
+              Salvar
+            </Button>
           </div>
         </div>
       </Modal>
--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,3 +1,4 @@
+import imageCompression from 'browser-image-compression';
 import { ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
@@ -13,3 +14,33 @@ export function sanitizeLink(link?: string) {
     .replace(/[!@#$%^&*()_+\-=\[\]{};':"\\|,ˆ.<>\/?]+/, '')
     .toLocaleLowerCase();
 }
+
+export async function compressFiles(files: File[]) {
+  const compressPromises = files.map(async file => {
+    try {
+      return await compressImage(file);
+    } catch (error) {
+      console.error('Error compressing image:', error);
+      return null;
+    }
+  });
+  return (await Promise.all(compressPromises)).filter(file => file !== null);
+}
+
+export const compressImage = async (file: File): Promise<File> => {
+  const options = {
+    maxSizeMB: 0.2,
+    maxWidthOrHeight: 900,
+    useWebWorker: true,
+    fileType: 'image/png',
+  };
+  return new Promise((resolve, reject) => {
+    imageCompression(file, options)
+      .then(compressedFile => {
+        resolve(compressedFile);
+      })
+      .catch(error => {
+        reject(error);
+      });
+  });
+};
